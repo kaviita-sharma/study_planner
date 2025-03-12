0e1e3a81b52c5ef855e5de77cd833ac8490e413b
Kavita-Sharma <kavita.sharma@unthinable.co>
2025-03-12 11:57:52 +0530

    merges

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,11 +1,16 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import API_BASE_URL from "../utils/config";
 import axios from "axios";
 
 // Thunk to get the user's profile
 export const getProfile = createAsyncThunk("auth/getProfile", async () => {
+<<<<<<< Updated upstream
   const response = await axios.get(
     "https://bcd7-2401-4900-1c52-18be-10e1-866a-521b-6a54.ngrok-free.app/api/auth/profile"
   );
+=======
+  const response = await axios.get(`${API_BASE_URL}/api/auth/profile`);
+>>>>>>> Stashed changes
   return response.data; // This returns a UserProfileResponse
 });
 
@@ -13,10 +18,14 @@ export const getProfile = createAsyncThunk("auth/getProfile", async () => {
 export const login = createAsyncThunk(
   "auth/login",
   async (credentials, { dispatch }) => {
+<<<<<<< Updated upstream
     const response = await axios.post(
       "https://bcd7-2401-4900-1c52-18be-10e1-866a-521b-6a54.ngrok-free.app/api/auth/login",
       credentials
     );
+=======
+    const response = await axios.post(`${API_BASE_URL}/api/auth/login`, credentials);
+>>>>>>> Stashed changes
     const data = response.data; // AuthResponse
     if (data.Success) {
       // Set the token in axios for subsequent calls
@@ -30,6 +39,7 @@ export const login = createAsyncThunk(
 // Thunk for user registration
 export const register = createAsyncThunk(
   "auth/register",
+<<<<<<< Updated upstream
   async (userData, { dispatch }) => {
     const response = await axios.post(
       "https://bcd7-2401-4900-1c52-18be-10e1-866a-521b-6a54.ngrok-free.app/api/auth/register",
@@ -39,10 +49,29 @@ export const register = createAsyncThunk(
     if (data.Success) {
       axios.defaults.headers.common["Authorization"] = `Bearer ${data.Token}`;
       dispatch(getProfile());
+=======
+  async (userData, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/api/auth/register`, userData);
+      const data = response.data; // AuthResponse
+
+      if (data.Success) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${data.Token}`;
+        dispatch(getProfile());
+      }
+      return data;
+    } catch (error) {
+      if (error.response && error.response.status == 400) {
+        return rejectWithValue(error.response?.data?.message ||
+           "Registration failed. Please try again."
+      ); // Send error message to Redux
+      }
+      return rejectWithValue("An unexpected error occurred.");
+>>>>>>> Stashed changes
     }
-    return data;
   }
 );
+
 
 const authSlice = createSlice({
   name: "auth",
---
+++ src/utils/config.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = " https://3927-157-119-213-251.ngrok-free.app"; // Change this to your actual API URL
+
+export default API_BASE_URL;
